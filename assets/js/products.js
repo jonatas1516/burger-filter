a81c53df13355004d1ba5d7397c35071c4a4b4f7
jonatas1516 <jonatas1516@gmail.com>
2024-01-18 12:49:06 -0300

    create list products

---
+++ assets/js/products.js
@@ -0,0 +1,43 @@
+const products = [
+  {
+    name: "X-Salada",
+    price: 30,
+    vegan: false,
+    src: "./assets/img/x-salad.jpeg",
+  },
+
+  {
+    name: "X-Bacon",
+    price: 34,
+    vegan: false,
+    src: "./assets/img/x-bacon.png",
+  },
+
+  {
+    name: "Bacon Egg",
+    price: 39,
+    vegan: false,
+    src: "./assets/img/bacon-egg.png",
+  },
+
+  {
+    name: "Monster",
+    price: 50,
+    vegan: false,
+    src: "./assets/img/monster.png",
+  },
+
+  {
+    name: "Big Vegano",
+    price: 55,
+    vegan: true,
+    src: "./assets/img/big-vegan.png",
+  },
+
+  {
+    name: "X-Vegan",
+    price: 45,
+    vegan: true,
+    src: "./assets/img/x-vegan.png",
+  },
+];
